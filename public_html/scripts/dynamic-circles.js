b0db6f59bee5f13b161f840e43ebc74c32e0a527
James Richie <james.richie.sulaeman@gmail.com>
2022-10-13 16:21:46 -0700

    Implemented dynamic homepage bubbles

---
+++ public_html/scripts/dynamic-circles.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+  var bubble1 = document.getElementById("bubble1");
+  document.body.addEventListener("mousemove", function (e) {
+    (bubble1.style.left = "calc(7.5em - " + 0.06 * (e.clientX - 500) + "px)"),
+      (bubble1.style.top = "calc(13em - " + 0.06 * (e.clientY - 500) + "px)");
+  });
+
+  var bubble2 = document.getElementById("bubble2");
+  document.body.addEventListener("mousemove", function (e) {
+    (bubble2.style.left = "calc(18.5em - " + 0.04 * (e.clientX - 500) + "px)"),
+      (bubble2.style.top = "calc(15em - " + 0.04 * (e.clientY - 500) + "px)");
+  });
+
+  var bubble3 = document.getElementById("bubble3");
+  document.body.addEventListener("mousemove", function (e) {
+    (bubble3.style.left = "calc(34.5em - " + 0.03 * (e.clientX - 500) + "px)"),
+      (bubble3.style.top = "calc(22em - " + 0.03 * (e.clientY - 500) + "px)");
+  });
+
+  var bubble4 = document.getElementById("bubble4");
+  document.body.addEventListener("mousemove", function (e) {
+    (bubble4.style.left = "calc(17.5em - " + 0.04 * (e.clientX - 500) + "px)"),
+      (bubble4.style.top = "calc(31em - " + 0.04 * (e.clientY - 500) + "px)");
+  });
+
+  var bubble5 = document.getElementById("bubble5");
+  document.body.addEventListener("mousemove", function (e) {
+    (bubble5.style.left = "calc(29.5em - " + 0.03 * (e.clientX - 500) + "px)"),
+      (bubble5.style.top = "calc(35em - " + 0.03 * (e.clientY - 500) + "px)");
+  });
+
+  var bubble6 = document.getElementById("bubble6");
+  document.body.addEventListener("mousemove", function (e) {
+    (bubble6.style.left = "calc(7.5em - " + 0.06 * (e.clientX - 500) + "px)"),
+      (bubble6.style.top = "calc(43em - " + 0.06 * (e.clientY - 500) + "px)");
+  });
+};

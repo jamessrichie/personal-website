677c48bf61210ffc3f1271ab4ce2825c66152a86
James Richie <james.richie.sulaeman@gmail.com>
2022-10-14 23:33:14 -0700

    Finished with about-me section

--- public_html/scripts/toggle-navbar.js
+++ public_html/scripts/toggle-navbar.js
@@ -1,15 +1,23 @@
-function toggleNavbar() {
-  const toggle = document.getElementsByClassName("navbar-toggle");
-  const banner = document.getElementsByClassName("navbar-banner");
-  const content = document.getElementsByClassName("navbar-content");
+const toggle = document.getElementsByClassName("navbar-toggle");
+const banner = document.getElementsByClassName("navbar-banner");
+const content = document.getElementsByClassName("navbar-content");
 
+function toggleNavbar() {
   if (toggle[0].classList.contains("navbar-toggle-hide")) {
-    toggle[0].classList.remove("navbar-toggle-hide");
-    banner[0].classList.remove("navbar-banner-hide");
-    content[0].classList.remove("navbar-content-hide");
+    showNavbar();
   } else {
-    toggle[0].classList.add("navbar-toggle-hide");
-    banner[0].classList.add("navbar-banner-hide");
-    content[0].classList.add("navbar-content-hide");
+    hideNavbar();
   }
+}
+
+function hideNavbar() {
+  toggle[0].classList.add("navbar-toggle-hide");
+  banner[0].classList.add("navbar-banner-hide");
+  content[0].classList.add("navbar-content-hide");
+}
+
+function showNavbar() {
+  toggle[0].classList.remove("navbar-toggle-hide");
+  banner[0].classList.remove("navbar-banner-hide");
+  content[0].classList.remove("navbar-content-hide");
 }

e4c5d3d36dea8fa2963b53d90c3e4b21665959d0
James Richie <james.richie.sulaeman@gmail.com>
2022-11-03 23:25:16 -0700

    Minor aesthetic changes

--- public_html/scripts/send-mail.js
+++ public_html/scripts/send-mail.js
@@ -1,4 +1,6 @@
 function sendMail() {
   var body = document.getElementById("input-form").value;
-  window.location.href = "mailto:james.richie.sulaeman@gmail.com?body=" + body;
+  window.location.href =
+    "mailto:james.richie.sulaeman@gmail.com?subject=Hey James! Let's Get In Touch&body=" +
+    body;
 }
